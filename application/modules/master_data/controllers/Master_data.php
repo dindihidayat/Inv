f725e32b5dd7eafb4cb3d535a71ccc38e38581f6
dindihidayat <dindihidayat@gmail.com>
2018-06-28 18:51:42 +0700

    Update master data error

--- application/modules/master_data/controllers/Master_data.php
+++ application/modules/master_data/controllers/Master_data.php
@@ -129,28 +129,28 @@ class Master_data extends MX_Controller {
     
     if ( ! $this->upload->do_upload('gambar')){
             $error = array('error' => $this->upload->display_errors());
-<<<<<<< .mine
+
             // print_r($error);
             // exit();
 
-=======
+
             // print_r($error);
             // exit();
             $gambar = $this->input->post('gambar_lama');
->>>>>>> .theirs
+
     }
     else{
             $data = array('upload_data' => $this->upload->data());
             $gambar = $data['upload_data']['file_name'];
-<<<<<<< .mine
+
             // print_r($data);
             // exit();
 
-=======
+
             // print_r($data);
             // exit();
             unlink('upload/gambar/'.$this->input->post('gambar_lama'));
->>>>>>> .theirs
+
     }
     $id = $this->input->post('idnya');
     $barang = ['kodebarang'=>$this->Masterdata_model->kode(),
